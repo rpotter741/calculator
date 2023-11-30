17cdf4422f5be4a8828fec2741e8fd9553e38632
rpotter741 <84155580+rpotter741@users.noreply.github.com>
2023-11-30 10:47:46 -0500

    Create baseline formatting
    - Used iOS calculator as baseline design inspiration;
    - Put nice rounded edges

---
+++ script.js
@@ -0,0 +1,4 @@
+//create stepStatus; everything is stepStatus 1 until an operator sign is hit, then it switches to 2. 
+//once the equals sign is hit, it clears array 1 and puts the solution in;
+//if an operator is hit && array1 is empty, then array1 will have the current solution put in;
+//array2 stays the same until an operator is hit, then it is cleared and waits for status2
